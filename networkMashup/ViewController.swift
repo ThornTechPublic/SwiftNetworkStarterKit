21bdab3bc1c6e4321491f1692e25c513514d95cb
Robert Chen <robert.chen@thorntechnologies.com>
2015-07-03 18:48:54 -0400

    Importing alamofire and taking it for a spin

--- networkMashup/ViewController.swift
+++ networkMashup/ViewController.swift
@@ -13,6 +13,13 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        request(.GET, "https://itunes.apple.com/us/rss/topfreeapplications/limit=10/json", parameters: nil, encoding: .JSON)
+            .responseJSON { (request, response, JSON, error) in
+                println("JSON:: \(JSON)")
+                
+            }
+        
     }
 
     override func didReceiveMemoryWarning() {
