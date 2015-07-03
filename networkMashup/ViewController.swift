20f596d94030e593c0c42b90c0897fb4ba736f93
Robert Chen <robert.chen@thorntechnologies.com>
2015-07-03 18:53:31 -0400

    Adding swifty json

--- networkMashup/ViewController.swift
+++ networkMashup/ViewController.swift
@@ -15,9 +15,10 @@ class ViewController: UITableViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         request(.GET, "https://itunes.apple.com/us/rss/topfreeapplications/limit=10/json", parameters: nil, encoding: .JSON)
-            .responseJSON { (request, response, JSON, error) in
-                println("JSON:: \(JSON)")
-                
+            .responseJSON { (request, response, data, error) in
+                let json = JSON(data!)
+                let iTunesStore = json["feed"]["author"]["name"]["label"].string
+                println("iTunesStore:: \(iTunesStore)")
             }
         
     }
