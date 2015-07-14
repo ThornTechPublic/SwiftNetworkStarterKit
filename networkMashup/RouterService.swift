a1d5f87064fefd76bafa8627327d76cb250ae87c
Robert Chen <robert.chen@thorntechnologies.com>
2015-07-14 18:33:21 -0400

    Removing this extension which was used for the old multipart implementation

--- networkMashup/RouterService.swift
+++ networkMashup/RouterService.swift
@@ -239,20 +239,3 @@ extension Request {
         })
     }
 }
-
-// MARK: - NSMutableData Extension
-
-// see http://stackoverflow.com/questions/26162616/upload-image-with-parameters-in-swift
-extension NSMutableData {
-    
-    /// Append string to NSMutableData
-    ///
-    /// Rather than littering my code with calls to `dataUsingEncoding` to convert strings to NSData, and then add that data to the NSMutableData, this wraps it in a nice convenient little extension to NSMutableData. This converts using UTF-8.
-    ///
-    /// :param: string       The string to be added to the `NSMutableData`.
-    
-    func appendString(string: String) {
-        let data = string.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: true)
-        appendData(data!)
-    }
-}
